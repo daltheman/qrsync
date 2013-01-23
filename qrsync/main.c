ca063683d90344ab1b9dd0342e29a2628d9d9ea6
Danilo Altheman <daltheman@gmail.com>
2013-01-23 00:11:43 -0200

    Initial Commit

---
+++ qrsync/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  qrsync
+//
+//  Created by Danilo Altheman on 23/01/13.
+//  Copyright (c) 2013 Danilo Altheman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
